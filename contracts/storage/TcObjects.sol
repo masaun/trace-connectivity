4431388d4a7d28d47f12626d47bc9e9843a8cc83
masaun <masanoriuno@gmail.com>
2019-08-05 10:57:24 +0200

    Add struct of District

--- contracts/storage/TcObjects.sol
+++ contracts/storage/TcObjects.sol
@@ -3,6 +3,20 @@ pragma solidity ^0.5.0;
 
 contract TcObjects {
 
+    struct District {
+        string country;     // Name of country
+        string district;    // Name of district
+        address IspAddr;    // Current ISP address which provide connectivity at the moment
+        uint uploadSpeedCurrently;   // Current upload speed
+        uint downloadSpeedCurrently; // Current download speed
+        
+        uint uploadSpeedStantdard;
+        uint downloadSpeedStantdard;
+        bool isReachUploadSpeedStantdard;
+        bool isReachDownloadSpeedStantdard;
+    }
+
+
     struct Isp {
         string name;
         address addr;
@@ -11,6 +25,7 @@ contract TcObjects {
         bool rightAsIsp;  // Right as ISP. If upload/download speed doesn't reach standard value, this value become false.
         uint timestamp;
     }
+
 
     struct Donor {
         string name;
