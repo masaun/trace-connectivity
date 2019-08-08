4130d29c4978869eebeab5f1cbe3a93e70399dcd
masaun <masanoriuno@gmail.com>
2019-08-08 20:01:34 +0200

    Remove extra properties from struc t of School

--- contracts/storage/TcObjects.sol
+++ contracts/storage/TcObjects.sol
@@ -7,13 +7,8 @@ contract TcObjects {
         string countryName;     // Name of country
         string schoolName;    // Name of district
         address IspAddr;        // Current ISP address which provide connectivity at the moment
-        uint uploadSpeedCurrently;   // Current upload speed
-        uint downloadSpeedCurrently; // Current download speed
-        
         uint uploadSpeedStantdard;
         uint downloadSpeedStantdard;
-        bool isReachUploadSpeedStantdard;   // If it is true, it meets specific school connectivity of upload speed
-        bool isReachDownloadSpeedStantdard; // If it is true, it meets specific school connectivity of download speed
 
         uint timestamp;
     }
