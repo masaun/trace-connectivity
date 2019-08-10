8c39b03377a2b4771c9a839dbcf1b47a8291c824
masaun <masanoriuno@gmail.com>
2019-08-10 09:33:54 +0200

    Fix code of total amount by using SafeMath

--- contracts/TraceConnectivityFund.sol
+++ contracts/TraceConnectivityFund.sol
@@ -1,5 +1,6 @@
 pragma solidity >=0.4.22 <0.6.0;
 
+import "./openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./storage/TcStorage.sol";
 import "./modifiers/TcOwnable.sol";
 import "./TraceConnectivityRegistry.sol";
@@ -8,13 +9,14 @@ import "./TraceConnectivityRegistry.sol";
 /* dev Donors donate in this contract */ 
 contract TraceConnectivityFund is TcStorage, TcOwnable, TraceConnectivityRegistry {
 
+    using SafeMath for uint256;
+
     constructor() public {}
 
 
     function fundFromDonor(
         address _donorAddr,
-        uint _fundAmountFromDonor,
-        uint _fundTotalAmount
+        uint _fundAmountFromDonor
     ) 
         public
         //onlyDonor(_donorAddr)
@@ -24,7 +26,7 @@ contract TraceConnectivityFund is TcStorage, TcOwnable, TraceConnectivityRegistr
         Fund storage fund = funds[_donorAddr];
         fund.donorAddr = _donorAddr;
         fund.fundAmountFromDonor = _fundAmountFromDonor;
-        fund.fundTotalAmount = _fundTotalAmount;
+        fund.fundTotalAmount = fund.fundTotalAmount.add(_fundAmountFromDonor);
 
         emit FundFromDonor(
             fund.donorAddr,
