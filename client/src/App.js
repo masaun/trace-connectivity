2f87ed04e8f3197546b163d5b10e3ca10fece531
masaun <masanoriuno@gmail.com>
2019-08-14 11:49:42 +0200

    [Temporarily] Move 'School Id Search' and 'Status of specific school connectivity' to page of school registry

--- client/src/App.js
+++ client/src/App.js
@@ -474,45 +474,6 @@ class App extends Component {
   }
 
   renderSchool() {
-    return (
-      <div className={styles.wrapper}>
-      {!this.state.web3 && this.renderLoader()}
-      {this.state.web3 && !this.state.trace_connectivity && (
-        this.renderDeployCheck('trace_connectivity')
-      )}
-      {this.state.web3 && this.state.trace_connectivity && (
-        <div className={styles.contracts}>
-          <h1>This page can register specific school</h1>
-          <div className={styles.widgets}>
-            <Card width={'350px'} bg="primary">
-              <h2>School Registry</h2>
-              <p>Country name</p>
-              <Input type="text" value={this.state.valueOfCountryName} onChange={this.handleInputCountryName} />
-
-              <p>School name</p>
-              <Input type="text" value={this.state.valueOfSchoolName} onChange={this.handleInputSchoolName} />
-
-              <p>Assign address of ISP (which this school want to give right of providing connectivity)</p>
-              <Input type="text" value={this.state.valueOfIspAddr} onChange={this.handleInputIspAddr} />
-
-              <p>Stantdard value of upload speed</p>
-              <Input type="text" value={this.state.valueOfUploadSpeedStantdard} onChange={this.handleInputUploadSpeedStantdard} />
-
-              <p>Stantdard value of download speed</p>
-              <Input type="text" value={this.state.valueOfDownloadSpeedStantdard} onChange={this.handleInputDownloadSpeedStantdard} />
-
-              <br />
-              
-              <Button onClick={this.sendSchoolRegister}>School Register</Button>
-            </Card>
-          </div>
-        </div>
-      )}
-      </div>
-    );
-  }
-
-  renderSchoolConnectivity() {
     const { accounts, trace_connectivity, uploadSpeedCurrently, downloadSpeedCurrently, realTimeDataList, schoolList, schoolId,schoolName, countryName, ispAddr, uploadSpeedStantdard, downloadSpeedStantdard } = this.state;
 
     const dataUploadSpeed = [
@@ -542,12 +503,27 @@ class App extends Component {
       {this.state.web3 && this.state.trace_connectivity && (
         <div className={styles.contracts}>
           <div className={styles.widgets}>
-            <BrowserRouter>
-              <Switch>
-                <Route path="/admin" render={props => <AdminLayout {...props} />} />
-                <Redirect from="/" to="/admin/dashboard/connectivity" />
-              </Switch>
-            </BrowserRouter>
+            <Card width={'350px'} bg="primary">
+              <h2>School Registry</h2>
+              <p>Country name</p>
+              <Input type="text" value={this.state.valueOfCountryName} onChange={this.handleInputCountryName} />
+
+              <p>School name</p>
+              <Input type="text" value={this.state.valueOfSchoolName} onChange={this.handleInputSchoolName} />
+
+              <p>Assign address of ISP (which this school want to give right of providing connectivity)</p>
+              <Input type="text" value={this.state.valueOfIspAddr} onChange={this.handleInputIspAddr} />
+
+              <p>Stantdard value of upload speed</p>
+              <Input type="text" value={this.state.valueOfUploadSpeedStantdard} onChange={this.handleInputUploadSpeedStantdard} />
+
+              <p>Stantdard value of download speed</p>
+              <Input type="text" value={this.state.valueOfDownloadSpeedStantdard} onChange={this.handleInputDownloadSpeedStantdard} />
+
+              <br />
+              
+              <Button onClick={this.sendSchoolRegister}>School Register</Button>
+            </Card>
 
             <Card width={'400px'} bg="primary">
 
@@ -592,6 +568,119 @@ class App extends Component {
               <p>Current value of download speed</p>
               { downloadSpeedCurrently }
             </Card>
+
+            <Card width={'750px'} bg="primary">
+              <ResponsiveContainer width="80%" height="40%" minWidth={600} minHeight={400}>
+                <LineChart data={dataUploadSpeed}>
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Line dataKey="uploadSpeedStandard" stroke="#FF0000" />
+                  <Line dataKey="uploadSpeedCurrently" stroke="#82ca9d" />
+                </LineChart>
+              </ResponsiveContainer>
+
+              <br />
+
+              <ResponsiveContainer width="80%" height="40%" minWidth={600} minHeight={400}>
+                <LineChart data={dataDownloadSpeed}>
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                  <Line dataKey="downloadSpeedStandard" stroke="#FF0000" />
+                  <Line dataKey="downloadSpeedCurrently" stroke="#8884d8" />
+                </LineChart>
+              </ResponsiveContainer>
+            </Card>
+          </div>
+        </div>
+      )}
+      </div>
+    );
+  }
+
+  renderSchoolConnectivity() {
+    /*
+    const { accounts, trace_connectivity, uploadSpeedCurrently, downloadSpeedCurrently, realTimeDataList, schoolList, schoolId,schoolName, countryName, ispAddr, uploadSpeedStantdard, downloadSpeedStantdard } = this.state;
+
+    const dataUploadSpeed = [
+      { name: '8/7', uploadSpeedStandard: 3.0, uploadSpeedCurrently: 3.3 },
+      { name: '8/8', uploadSpeedStandard: 3.0, uploadSpeedCurrently: 3.5 },
+      { name: '8/9', uploadSpeedStandard: 3.0, uploadSpeedCurrently: 2.9 },
+      { name: '8/10', uploadSpeedStandard: 3.0, uploadSpeedCurrently: 3.1 },
+      { name: '8/11', uploadSpeedStandard: 3.0, uploadSpeedCurrently: 2.7 },
+      { name: '8/12', uploadSpeedStandard: 3.0, uploadSpeedCurrently: 2.9 }
+    ]
+
+    const dataDownloadSpeed = [
+      { name: '8/7', downloadSpeedStandard: 3.0, downloadSpeedCurrently: 2.1 },
+      { name: '8/8', downloadSpeedStandard: 3.0, downloadSpeedCurrently: 3.1 },
+      { name: '8/9', downloadSpeedStandard: 3.0, downloadSpeedCurrently: 3.2 },
+      { name: '8/10', downloadSpeedStandard: 3.0, downloadSpeedCurrently: 3.3 },
+      { name: '8/11', downloadSpeedStandard: 3.0, downloadSpeedCurrently: 3.2 },
+      { name: '8/12', downloadSpeedStandard: 3.0, downloadSpeedCurrently: 2.8 }
+    ]
+    */
+
+    return (
+      <div className={styles.wrapper}>
+      {!this.state.web3 && this.renderLoader()}
+      {this.state.web3 && !this.state.trace_connectivity && (
+        this.renderDeployCheck('trace_connectivity')
+      )}
+      {this.state.web3 && this.state.trace_connectivity && (
+        <div className={styles.contracts}>
+          <div className={styles.widgets}>
+            <BrowserRouter>
+              <Switch>
+                <Route path="/admin" render={props => <AdminLayout {...props} />} />
+                <Redirect from="/" to="/admin/dashboard/connectivity" />
+              </Switch>
+            </BrowserRouter>
+
+            {/*
+            <Card width={'400px'} bg="primary">
+
+              <Button onClick={this.getRealTimeData}>Get Real-Time Data</Button>
+
+              <hr />
+
+              <h2>School Id Search</h2>
+              <Input type="text" value={this.state.valueOfSchoolId} onChange={this.handleInputSchoolId} />
+
+              <br />
+
+              <Button onClick={this.sendSchoolDetail}>Search School Detail</Button>
+
+              <span style={{ padding: "20px" }}></span>
+
+              <hr />
+
+              <h2>Status of specific school connectivity</h2>
+              <p>Country name</p>
+              { countryName }
+
+              <p>School name</p>
+              { schoolName }
+
+              <p>Assigned ISP address (currently)</p>
+              { ispAddr }
+
+              <hr />
+
+              <p>Stantdard value of upload speed</p>
+              { uploadSpeedStantdard }
+
+              <p>Current value of upload speed</p>
+              { uploadSpeedCurrently }
+
+              <hr />
+ 
+              <p>Stantdard value of download speed</p>
+              { downloadSpeedStantdard }
+
+              <p>Current value of download speed</p>
+              { downloadSpeedCurrently }
+            </Card>
+            */}
 
             {/*
             <Card width={'750px'} bg="primary">
